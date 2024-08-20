a4fde79056b44aa902ee7a894e92dd0abbf47e46
Luis-Romero1 <60513896+Luis-Romero1@users.noreply.github.com>
2024-08-20 07:53:16 -0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -10,17 +10,20 @@ endpoint_name="Iris_endpoint"
 rolef = os.getenv("ROLL_IAM")
 
 
-# try:
-#     vpc_configf = {
-#         'Subnets': [os.getenv("SUBPRI"), os.getenv("SUBDB")],  # Tus subnets
-#         'SecurityGroupIds': [os.getenv("SGIN"),os.getenv("SGOUT")]  # Tus security groups
-#     }
-# except:
-#     vpc_configf = {
-#         'Subnets': ["subnet-0481d2f1cb01b8236", "subnet-064f2feaf0c3d4c0a"],  # Tus subnets
-#         'SecurityGroupIds': ["sg-0c4847fce2396e1be","sg-0078bd29f6e0aa50c"]  # Tus security groups
-#     }
+try:
+    vpc_configf = {
+        'Subnets': [os.getenv("SUBPRI"), os.getenv("SUBDB")],  # Tus subnets
+        'SecurityGroupIds': [os.getenv("SGIN"),os.getenv("SGOUT")]  # Tus security groups
+    }
+except:
+    vpc_configf = {
+        'Subnets': ["subnet-0481d2f1cb01b8236", "subnet-064f2feaf0c3d4c0a"],  # Tus subnets
+        'SecurityGroupIds': ["sg-0c4847fce2396e1be","sg-0078bd29f6e0aa50c"]  # Tus security groups
+    }
 
+training_image_config = {
+    'TrainingRepositoryAccessMode': 'VPC'
+}
 # Definir el estimador y entrenar
 sklearn_estimator = SKLearn(
     entry_point="main.py",  # Archivo principal que se ejecutará
@@ -31,10 +34,11 @@ sklearn_estimator = SKLearn(
     framework_version=FRAMEWORK_VERSION,
     base_job_name="Custom-iris-sklearn",
     use_spot_instances=True,
-    # vpc_config=vpc_configf,
+    vpc_config=vpc_configf,
     max_wait=7200,
     max_run=3600,
-    dependencies=['requirements.txt']  # Asegura la instalación de las dependencias
+    dependencies=['requirements.txt'],
+    training_image_config=training_image_config  # Asegura la instalación de las dependencias
 )
 # Entrenamiento del modelo
 sklearn_estimator.fit(wait=True)
@@ -50,7 +54,8 @@ model = SKLearnModel(
     source_dir=".",  # Directorio actual contiene todos los archivos necesarios
     framework_version=FRAMEWORK_VERSION,
     dependencies=['requirements.txt'],  # Asegura la instalación de las dependencias
-    # vpc_config=vpc_configf  # Opcional, si tienes VPC configurado
+    vpc_config=vpc_configf,
+    training_image_config=training_image_config  # Opcional, si tienes VPC configurado
 )
 
 # Desplegar el modelo como un endpoint
